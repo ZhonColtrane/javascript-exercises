b26eff090320201f66c309f105e33744409459e4
Zi Tian Zhang <116614020+ZhonColtrane@users.noreply.github.com>
2025-12-26 05:49:24 -0500

    Complete exercise using array.sort() method.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+function sumAll(int1, int2) {
+    if (int1 < 0 || int2 < 0 || 
+        !Number.isInteger(int1) || !Number.isInteger(int2)) {
+        return "ERROR";
+    }
+    let arr = [int1, int2];
+    arr.sort((int1, int2) => int1 - int2);
+    let sum = 0;
+    for (let i = arr[0]; i <= arr[1]; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
