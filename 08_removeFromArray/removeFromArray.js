6b4df0e03d04226b91cf1a02aa37e55ea01a008d
Zi Tian Zhang <116614020+ZhonColtrane@users.noreply.github.com>
2025-12-26 05:33:51 -0500

    Complete exercise with different solution

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for (arg of args) {
+        for (let i = 0; i < arr.length; i++) {
+                if (arr[i] === arg) {
+                    arr.splice(i,1);
+                    i--;
+                }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
