12488498d5fecf26bff4d63579a73f41585b97b1
Zi Tian Zhang <116614020+ZhonColtrane@users.noreply.github.com>
2026-01-04 18:04:43 -0500

    Complete exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return numbers.reduce((total, number) => total + number, 0);	
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prod,num) => prod*num,1);
 };
 
-const power = function() {
-	
+const power = function(base,exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for (let i = 1; i<=num; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
