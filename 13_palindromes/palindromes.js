db4490115b86a233c2a755cabf588f8d0b09f842
Zi Tian Zhang <116614020+ZhonColtrane@users.noreply.github.com>
2026-01-04 18:51:38 -0500

    Complete exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const validChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const lowerCase = string.toLowerCase();
+    const cleanedArr = lowerCase
+        .split('')
+        .filter(char => validCharArray.includes(char))
+    const copy = [...cleanedArr];
+    const reversed = copy.reverse();
+    return cleanedArr.join() === reversed.join();
 };
-
 // Do not edit below this line
 module.exports = palindromes;
+
