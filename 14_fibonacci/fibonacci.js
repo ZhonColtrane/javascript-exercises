5dbbed0809cc48c4bb9b8ad64cec44210e5dc465
Zi Tian Zhang <116614020+ZhonColtrane@users.noreply.github.com>
2026-01-04 19:30:34 -0500

    Complete exercise both ways

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(count){
+    let array = [0,1];
+    let countInt = parseInt(count);
+    if (countInt < 0){
+        return "OOPS";
+    } if (array[countInt] === undefined){
+        for (i=2; i<=countInt; i++) {
+            let fibonacci = array[i-1] + array[i-2];
+            array.push(fibonacci);
+        }
+    }      
+    return array[countInt]
+    }
 
 // Do not edit below this line
 module.exports = fibonacci;
