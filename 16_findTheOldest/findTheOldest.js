523b5d585c66a6eddc2e216e0997cd5a2e068981
Zi Tian Zhang <116614020+ZhonColtrane@users.noreply.github.com>
2026-01-04 22:00:46 -0500

    Complete exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    for (person of array){
+        const thisYear = new Date().getFullYear()
+        if (person.yearOfDeath == undefined) {
+            person.yearOfDeath = thisYear
+        }
+        person.age = person.yearOfDeath - person.yearOfBirth;
+    }
+    array.sort((a,b) => b.age - a.age);
+    return array[0];
 };
 
 // Do not edit below this line
